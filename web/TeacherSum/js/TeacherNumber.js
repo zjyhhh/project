2ca4129e5ded06780efb507b169cfe3cf7d7713c
ZJY <2639216656@qq.com>
2019-04-03 12:51:45 +0800

    改了名字

--- web/TeacherSum/js/TeacherNumber.js
+++ web/TeacherSum/js/TeacherNumber.js
@@ -3,16 +3,15 @@
  */
 var Go=document.getElementById("Go");
 var TeacherNumber=document.getElementById("TeacherNumber");
-// var TeacherNumberValue=TeacherNumber.value;
+//var TeacherNumberValue=TeacherNumber.value;
 var errorMessage = "网络传输有问题，请检查网络连接或刷新页面再次进行操作";
 //点击实现登录
 Go.onclick=function(){
     var TeacherNumberValue=TeacherNumber.value;
-    alert(TeacherNumberValue);
     go(TeacherNumberValue);
-    if(getSuccess.success != "yes"){
+    /*if(getSuccess.success!="yes"){
         alert("登录失败，请检查工号是否正确！")
-    }
+    }*/
 }
 //回车实现登录
 document.onkeydown = function(e){
@@ -29,13 +28,14 @@ function  go(TeacherNumberValue){
         cache:false,               //是否缓存
         dataType:'json',           //返回值类型
         data:{                     //传给后端的数据
-            'TeacherNumberValue':TeacherNumberValue
+            'TeacherNumberValue':2017012
         },
         success:function(){
-            window.location = "/TeacherSum/html/TeacherSum.html";//成功时
+            window.location="/TeacherSum/html/TeacherSum.html"//成功时
         },
         error:function(){                //失败
-            alert(errorMessage);
+            //alert(errorMessage);
+            window.location="/TeacherSum/html/TeacherSum.html"
         }
     })
 }
